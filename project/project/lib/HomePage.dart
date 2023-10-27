a3e3af15f4b0362ba19b2241c35183a2da2a2d1a
Raghad1M <147823679+Raghad1M@users.noreply.github.com>
2023-10-27 15:08:21 +0300

    enhancement

--- project/project/lib/HomePage.dart
+++ project/project/lib/HomePage.dart
@@ -1,147 +1,93 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  List<Widget> _widgetOptions = [
+    NewWidget(),
+    Text('Profile Page Content'),
+    Text('Courses Catalog Page Content'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const NewWidget();
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt),
+            label: 'Courses',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
 
 class NewWidget extends StatelessWidget {
   const NewWidget({
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home: Scaffold(
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 18, 19, 102),
-      ),  
-
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
+            const SizedBox(height: 20),
+            const Text(
+              'My Courses',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            ListTile(
+              title: const Text('Algorithms design'),
+              onTap: () {
+                // Navigate to the Algorithms design course page
               },
-              child: const Text('My Courses',),
-            
-          
             ),
-             Container(//text
-              width:500,
-              height:50,
-              color: const Color.fromARGB(255, 255, 255, 255),
-              margin: const EdgeInsets.fromLTRB(10, 10, 200, 0),
-              padding: const EdgeInsets.fromLTRB(1, 20, 0, 0),
-              child:  const Text('Algorithms design'),
-              
-            ),
-            Container(//img
-              width:500 ,
-              height: 200,
-              color: const Color.fromARGB(255, 255, 255, 255),
-              margin: const EdgeInsets.symmetric(vertical:0,horizontal:0),
-              padding: const EdgeInsets.fromLTRB(1, 0, 0, 0),
-              child: Image.asset('assets/images/course-img2.png',width:500,height:100,),
-              
-            ),
-
-            Container(//text
-              width:500,
-              height:50,
-              color: const Color.fromARGB(255, 255, 255, 255),
-              margin: const EdgeInsets.fromLTRB(10, 20, 200, 0),
-              padding: const EdgeInsets.fromLTRB(1, 30, 0, 0),
-              child:  const Text('Web development'),
-              
-            ),
-            Container(//img
-              width:500 ,
-              height: 200,
-              color: const Color.fromARGB(255, 255, 255, 255),
-               margin: const EdgeInsets.fromLTRB(5, 0, 20, 0),
-              padding: const EdgeInsets.fromLTRB(1, 0, 0, 20),
-              child: Image.asset('assets/images/course-img3.png',width:500,height:100,),
-            ),
-
-            
-            ElevatedButton(
-              onPressed: () {
-              
+            Image.asset('assets/images/course-img2.png', width: 500, height: 200),
+            ListTile(
+              title: const Text('Software Engineering'),
+              onTap: () {
+                // Navigate to the Web development course page
               },
-              child: const Text('Available courses'),
             ),
-
-
-             Container(//text
-              width:500,
-              height:50,
-              color: const Color.fromARGB(255, 255, 255, 255),
-              margin: const EdgeInsets.fromLTRB(10, 10, 200, 0),
-              padding: const EdgeInsets.fromLTRB(1, 30, 0, 0),
-              child:  const Text('Networks'),
-              
-            ),
-            Container(//img
-              width:500 ,
-              height: 200,
-              color: const Color.fromARGB(255, 255, 255, 255),
-              margin: const EdgeInsets.symmetric(vertical:5,horizontal:0),
-              padding: const EdgeInsets.fromLTRB(1, 0, 0, 70),
-              child: Image.asset('images/tt.jpeg',width:500,height:100,),
-            ),
-
-             Container(//text
-              width:500,
-              height:50,
-              color: const Color.fromARGB(255, 255, 255, 255),
-              margin: const EdgeInsets.fromLTRB(10, 20, 200, 0),
-              padding: const EdgeInsets.fromLTRB(1, 30, 0, 0),
-              child:  const Text('Software engineering'),
-              
-              
-            ),
-            Container(//img
-              width:500 ,
-              height: 200,
-              color: const Color.fromARGB(255, 255, 255, 255),
-              margin: const EdgeInsets.symmetric(vertical:0,horizontal:0),
-              padding: const EdgeInsets.fromLTRB(1, 0, 0, 0),
-              child: Image.asset('images/database.jpg',width:500,height:100,),
-              
-            ),
-
-            Container(//text
-              width:500,
-              height:50,
-              color: const Color.fromARGB(255, 255, 255, 255),
-              margin: const EdgeInsets.fromLTRB(10, 20, 200, 0),
-              padding: const EdgeInsets.fromLTRB(1, 30, 0, 0),
-              child:  const Text('Data Base'),
-              
-            ),
-            Container(//img
-              width:500 ,
-              height: 200,
-              color: const Color.fromARGB(255, 255, 255, 255),
-               margin: const EdgeInsets.fromLTRB(5, 0, 20, 0),
-              padding: const EdgeInsets.fromLTRB(1, 0, 0, 20),
-              child: Image.asset('images/net2.jpeg',width:500,height:100,),
-            ),
-
-            
+            Image.asset('assets/images/course-img3.png', width: 500, height: 200),
+            // Add similar ListTiles and Images for other courses
           ],
         ),
       ),
-    ),
     );
   }
 }
--- project/project/lib/main.dart
+++ project/project/lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:project/chooserule.dart';
-import 'package:project/reg.dart';
-import 'firebase_options.dart'; // Only if you have this file
+import 'firebase_options.dart';
+
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Ensure you have widget bindings initialized
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  ); // Initialize Firebase
-  
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
